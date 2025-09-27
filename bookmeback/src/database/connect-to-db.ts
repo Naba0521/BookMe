282e39976b99611d44ebc1bf5e4120dad1a63387
Naba0521 <naranbaatar.m@nest.edu.mn>
2025-09-27 10:48:42 +0800

    mongodb url changed

--- bookmeback/src/database/connect-to-db.ts
+++ bookmeback/src/database/connect-to-db.ts
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 export const connectToDataBase = async () => {
   const mongoUri =
     process.env.MONGODB_URI ||
-    "mongodb+srv://naba:%40Pi26114@bookme.hbntwcq.mongodb.net/?retryWrites=true&w=majority&appName=BookMe";
+    "mongodb+srv://naba:@Pi26114@cluster0.cgzjdlf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   await connect(mongoUri);
   console.log("Connected mongodb database");
 };
